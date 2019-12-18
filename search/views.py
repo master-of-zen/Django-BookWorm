d9f1860a8f303ce7904e9e2beba3303982323a5f
Zen <46526140+master-of-zen@users.noreply.github.com>
2019-12-19 01:03:41 +0200

    Adding html and routing

--- search/views.py
+++ search/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+#from .models import
 # Create your views here.
+
+
+class MainView(View):
+    template_name = 'search/base.html'
