7782fe7a76dd05be5ff15fb039a65c9cfd80c3f7
Zen <46526140+master-of-zen@users.noreply.github.com>
2019-12-20 14:36:26 +0200

    + routing

---
+++ search/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.search.as_view(), name='search'),
+    path('about/', views.about.as_view(), name='search-about'),
+
+]
