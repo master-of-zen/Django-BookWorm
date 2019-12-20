5379e8f6da2a3511ec7448b82ab119a85e1880af
Zen <46526140+master-of-zen@users.noreply.github.com>
2019-12-20 14:31:23 +0200

    Renaming project

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booktime.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookworm.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
