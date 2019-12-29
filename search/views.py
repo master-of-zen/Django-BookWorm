da53de0115484c84149c6699f11a190e08b6a32d
Zen <46526140+master-of-zen@users.noreply.github.com>
2019-12-29 22:11:51 +0200

    get request for search form

--- search/views.py
+++ search/views.py
@@ -6,12 +6,14 @@ from django.http import HttpResponse
 class search(TemplateView):
 
     template_name = 'search/search.html'
-    """
+
     def get(self, request, *args, **kwargs):
-        q_ = request.GET['usr_query']
-        print(q_)
-        return render(request, 'search.html')
-    """
+        if request.GET.get('q'):
+            print(request.GET)
+            return render(request, 'search/search.html')
+        else:
+            return render(request, 'search/search.html')
+
 
 class about(TemplateView):
     template_name = 'search/about.html'
