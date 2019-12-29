0c99f6b4609c7f889c92bd3191d9d2ac21c6c09f
Zen <46526140+master-of-zen@users.noreply.github.com>
2019-12-29 21:44:15 +0200

    fix + work on

--- search/views.py
+++ search/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from django.http import HttpResponse
+
 
 class search(TemplateView):
-    template_name = 'search.html'
-    
+
+    template_name = 'search/search.html'
+    """
+    def get(self, request, *args, **kwargs):
+        q_ = request.GET['usr_query']
+        print(q_)
+        return render(request, 'search.html')
+    """
 
 class about(TemplateView):
-    template_name = 'about.html'
+    template_name = 'search/about.html'
