34d0981d3b0de8f50ffb6f2ee1c362a448464332
master-of-zen <46526140+master-of-zen@users.noreply.github.com>
2019-12-31 22:36:38 +0200

    search work

--- search/views.py
+++ search/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, FormView
 from django.http import HttpResponse
-from BookSearch.book_search import search
+from BookSearch.book_search import search as book_search
+
 
 class search(TemplateView):
 
@@ -9,10 +10,13 @@ class search(TemplateView):
 
     def get(self, request, *args, **kwargs):
         if request.GET.get('q'):
-            print(request.GET.get('q'))
-            print(search(request.GET.get('q')))
+            req = request.GET.get('q')
+            print(req)
+            search_book = book_search(req)
+            print(search_book)
             return render(request, 'search/search.html')
         else:
+            print('else')
             return render(request, 'search/search.html')
 
 
