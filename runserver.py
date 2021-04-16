e550ac5f453745e608c516ebc768c3133171cb9e
Zen <46526140+master-of-zen@users.noreply.github.com>
2021-04-16 19:09:18 +0300

    +1

--- runserver.py
+++ runserver.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import subprocess
 import os
-path = os.getcwd() + '/manage.py'
+
+path = os.getcwd() + "/manage.py"
 
 try:
-    subprocess.run([path, 'runserver'])
+    subprocess.run([path, "runserver"])
 
 except KeyboardInterrupt:
-    print(f'\n Server Stoped')
+    print(f"\n Server Stoped")
     os._exit()
